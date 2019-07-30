185d7bd9f0a44ecbfdfc461044c16d3da0796649
sheethal sebastian <sheethalsebastian2017@gmail.com>
2019-07-29 20:56:01 -0400

    adding hero images and adding pug files

--- models/survey.js
+++ models/survey.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const surveySchema = new Schema({
-    title: { type: String, required: true},
-    content: String,
-    date: Date,
-    authors: [String],
-    hero: String,
+  title: String,
+  datePosted: Date,
+  content: String,
+  authors: [String],
+  hero: String
 });
 
-const Survey = mongooose.model('Survey',surveySchema);
+const Survey = mongoose.model('Survey', surveySchema);
 
 module.exports = Survey;
